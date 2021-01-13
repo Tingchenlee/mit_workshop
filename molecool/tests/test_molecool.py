41627166f43045c29f354cb983bbb12659b3f1a9
Tingchenlee <lee.ting@northeastern.edu>
2021-01-13 10:41:29 -0500

    add homework files

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -8,19 +8,44 @@ import pytest
 import sys
 import numpy as np
 
+
 @pytest.fixture(scope="module")
 def methane_molecule():
-    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    symbols = np.array(["C", "H", "H", "H", "H"])
 
-    coordinates = np.array([
-        [1, 1, 1],
-        [2.4, 1, 1],
-        [-0.4, 1, 1],
-        [1, 1, 2.4],
-        [1, 1, -0.4]
-    ])
+    coordinates = np.array(
+        [[1, 1, 1], [2.4, 1, 1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]]
+    )
 
     return symbols, coordinates
+
+
+@pytest.mark.parametrize(
+    "p1, p2, p3, expected_value",
+    [
+        (
+            np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            45,
+        ),
+        (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+        (
+            np.array([np.sqrt(3) / 2, (1 / 2), 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            30,
+        ),
+    ],
+)
+def test_calculate_angle_many(p1, p2, p3, expected_value):
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_value == pytest.approx(
+        calculated_angle
+    ), f"{calculated_angle}{expected_value}"
+
 
 def test_calculate_angle():
 
@@ -33,6 +58,7 @@ def test_calculate_angle():
     calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
 
     assert expected_value == calculated_value
+
 
 @pytest.mark.skip
 def test_build_bond_list_failure(methane_molecule):
@@ -54,6 +80,7 @@ def test_build_bond_list(methane_molecule):
     for bond_length in bonds.values():
         assert bond_length == 1.4
 
+
 def test_calculate_distance():
     """Test t he calculate distance function calculates what we expect."""
 
@@ -70,3 +97,22 @@ def test_calculate_distance():
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
+
+    
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+@pytest.mark.skip
+def test_center_of_mass(methane_molecule):
+    symbols, coordinates = methane_molecule
+
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1,1,1])
+
+    assert np.array_equal(center_of_mass, expected_center)
